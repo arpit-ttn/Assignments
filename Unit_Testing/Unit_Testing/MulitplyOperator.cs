0e163945278b7330ed0ac926b510e6eb0f2a14a4
arpit-ttn <arpit.deora@tothenew.com>
2020-04-19 21:01:32 +0530

    Done

---
+++ Unit_Testing/Unit_Testing/MulitplyOperator.cs
@@ -0,0 +1,51 @@
+﻿using Bank;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unit_Testing
+{
+    public class MulitplyOperator
+    {
+        private double? _numericResult;
+        public MulitplyOperator()
+        {
+        }
+
+
+        //multiply numeric numbers
+        public object Evaluate(List<object> data)
+        {
+           
+            object result;
+            foreach (object value in data)
+            {
+                if (value.IsNumeric())
+                {
+
+                    if (_numericResult == null)
+                        _numericResult = Convert.ToDouble(value);
+                    else
+                    {
+                        _numericResult = _numericResult.Value * Convert.ToDouble(value);
+                    }
+
+                }
+                else
+                {
+                    throw new ArgumentException($"Non numeric or string value encountered");
+                }
+            }
+
+            if (_numericResult != null)
+            {
+                result = _numericResult.Value;
+            }
+            else
+            {
+                throw new ArgumentException($"Incorrect arguments encountered in {nameof(MulitplyOperator)}");
+            }
+            return result;
+        }
+    }
+}
