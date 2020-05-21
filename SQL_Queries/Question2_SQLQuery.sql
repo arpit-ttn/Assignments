43a558b4ef2e0482440caaacda2717157f94a18e
arpit-ttn <arpit.deora@tothenew.com>
2020-05-21 15:46:49 +0530

    Done

--- SQL_Queries/Question2_SQLQuery.sql
+++
@@ -1,4 +0,0 @@
------------------------------Q-2 How to update column name of table ?-----------------------------
-
-sp_rename 'Emp_Salaries.Salary', 'Salaries', 'COLUMN';
-Select*from Emp_Salaries
